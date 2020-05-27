e0677eb828fedd6c7846f587f32cc04ce4a345e4
Derick Moncado <derickmoncado@gmail.com>
2020-05-27 08:58:51 -0700

    Liking a tweet.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,16 +4,26 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
 	handleLike = (e) => {
 		e.preventDefault()
 		// todo: Handle Like Tweet
+		const { dispatch, tweet, authedUser } = this.props
+
+		dispatch(handleToggleTweet({
+			id: tweet.id,
+			hasLiked: tweet.hasLiked,
+			authedUser
+		}))
 	}
+
 	toParent = (e, id) => {
 		e.preventDefault()
 		// todo: Redirect to parent Tweet.
 	}
+	
 	render() {
 		const { tweet } = this.props
 
